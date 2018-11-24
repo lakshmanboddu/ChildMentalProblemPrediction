8d9f51a2dd5ba1a81347b8087a36fd9d054b000f
lakshmanboddu <lakshmanboddu36@gmail.com>
2018-11-24 23:25:32 +0600

    initial commit

---
+++ ChildMentalProblemPrediction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChildMentalProblemPrediction
+//
+//  Created by Saber Hossain on 24/11/18.
+//  Copyright © 2018 Lakshman Babu Boddu Lakshman Babu Boddu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
